b4a23a95f895f546f61f434aae287f73d6a06a52
NicklasHall <nicklas.hall@elev.ga.ntig.se>
2023-08-24 13:00:30 +0200

    aaa

---
+++ intro/intro.py
@@ -0,0 +1,22 @@
+#print("hello world")
+#print(20 + 20)
+#print(20 - 20)
+#print(20 / 20)
+
+'''
+x = input("namn: ")
+age = input("ålder: ")
+
+print(x)
+print(age)
+
+print("hej", x, "du är" , age, "år")
+'''
+
+print("""
+Hej Klassen 
+Detta är flerradning print""")
+
+print("Resultat:\n Teinf22b vann:\n leken")
+
+'''
---
+++ intro/module.py
@@ -0,0 +1,30 @@
+print("hello world")
+print("Hello \"world\"")
+
+print("""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum""")
+
+print("Jag heter Nicklas \n Isak är min vän \n Leo är min kusin")
+
+print("fy" + "17!")
+       
+x=("420")
+print("gata" , x)
+
+Klass = input("Class: ")
+print(Klass)
+
+print("Välkommen \n Skriv ditt namn och din ålder och land")
+a = input("Namn: ")
+b = input("ålder: ")
+c = input("Land: ")
+print(a,b,c)
